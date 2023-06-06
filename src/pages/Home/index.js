98c46f491a847fbd73f81d107c35080ea7e4dcbb
ARRARIAKU2m <yerry_kecak@yahoo.com>
2023-06-06 20:57:58 +0700

    add components folder

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./index.css";
-import img1 from "./assets/homebg.jpg"
 
 
 function Home() {
     return(
         <>
             <div className="bg-fixed">
-                <div className="bg-fixed">
-                    <h1>adfhsjdfh</h1>
-                </div>
+                
             </div>
         </>
     )
