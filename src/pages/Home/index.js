94057dddae386cc1cbaac93a191f633a1f6bf956
ARRARIAKU2m <yerry_kecak@yahoo.com>
2023-06-06 22:43:50 +0700

    yaamponn pusing :(

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,14 +7,21 @@ import img1 from "../../assets/homebg.jpg";
 function Home() {
     return (
         <>
-            <Navbar />
-
-            <div className="relative">
-                <img src="https://img1.hotstarext.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/8579/1498579-h-4dfb0de0e149" />
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <p className="text-white">Centered Text</p>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <div className="relative">
+                    <img src="https://img1.hotstarext.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/8579/1498579-h-4dfb0de0e149" />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <div className="productlogo inset-0 justify-center items-center">
+                            <p>Discover Your Favourite Movie and More</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </main>
+
+
         </>
     )
 }
