493414cf993d7210b4fffea1e3ff3663dd7ec592
ARRARIAKU2m <yerry_kecak@yahoo.com>
2023-06-07 13:49:33 +0700

    Fix Movie Box

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,34 +7,10 @@ import img1 from "../../assets/homebg.jpg";
 import { useEffect, useState } from "react";
 
 function Home() {
-    const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const response = await fetch(
-                    `https://api.themoviedb.org/3/movie/popular?api_key=1487e6da2b780246708f52464d6d0f05`
-                );
-                const data = await response.json();
-                setMovies(data.results);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-
-        fetchMovies();
-    }, []);
 
     return (
         <>
-            {/* <div>
-                <h1>Popular Movies</h1>
-                <ul>
-                    {movies.map((movie) => (
-                        <li key={movie.id}>{movie.title}</li>
-                    ))}
-                </ul>
-            </div> */}
             <header>
                 <Navbar />
             </header>
