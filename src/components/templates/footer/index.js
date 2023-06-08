7e0282e9f0a5f1c798ef39f4ba941af1f74e32b2
ARRARIAKU2m <yerry_kecak@yahoo.com>
2023-06-08 22:46:18 +0700

    Add Card Slider

--- src/components/templates/footer/index.js
+++ src/components/templates/footer/index.js
@@ -7,7 +7,7 @@ function Footer() {
                 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div class="md:flex md:justify-between">
                         <div class="mb-6 md:mb-0">
-                            <a href="https://flowbite.com/" class="flex items-center">
+                            <a href="/" class="flex items-center">
                                 <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
                                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DeMovies</span>
                             </a>
@@ -17,7 +17,7 @@ function Footer() {
                                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
                                 <ul class="text-gray-600 dark:text-gray-400 font-medium">
                                     <li class="mb-4">
-                                        <a href="https://flowbite.com/" class="hover:underline">DeMovies</a>
+                                        <a href="/" class="hover:underline">DeMovies</a>
                                     </li>
                                     <li>
                                         <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
